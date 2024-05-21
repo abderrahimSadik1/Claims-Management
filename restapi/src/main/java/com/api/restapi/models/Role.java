24d8f225c2388d2a7b385ec258d1ec171743beb6
Abderrahim Sadik <62406635+abderrahimSadik1@users.noreply.github.com>
2024-05-22 00:13:26 +0100

    Delete restapi directory

--- restapi/src/main/java/com/api/restapi/models/Role.java
+++
@@ -1,6 +0,0 @@
-package com.api.restapi.models;
-
-public enum Role {
-    ADMIN,
-    User
-}
--- restapi/src/main/java/com/api/restapi/models/Status.java
+++
@@ -1,7 +0,0 @@
-package com.api.restapi.models;
-
-public enum Status {
-    SENT,
-    APPROVED,
-    REJECTED
-}
